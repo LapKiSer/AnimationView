86a649d29a4ba8f0a9fae5ae4e805d995f664a42
LapKiSer <lapkovskiykirill@gmail.com>
2023-05-02 17:05:48 +0300

    Finish

---
+++ AnimationView/DataStore.swift
@@ -0,0 +1,60 @@
+//
+//  DataStore.swift
+//  AnimationView
+//
+//  Created by Кирилл Лапковский on 02.05.2023.
+//
+import Foundation
+
+struct LabelValue {
+    var preset: String
+    var curve: String
+    var force: CGFloat
+    var duration: CGFloat
+}
+
+let nameAnimations = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+]
+
+let nameCurves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic"
+]
--- AnimationView/ViewController.swift
+++ AnimationView/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SpringAnimation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     @IBOutlet var springView: SpringView!
     
     @IBOutlet var presetValue: UILabel!
@@ -16,14 +16,50 @@ class ViewController: UIViewController {
     @IBOutlet var forceValue: UILabel!
     @IBOutlet var durationValue: UILabel!
     @IBOutlet var delayValue: UILabel!
+        
+    private var labelValue = LabelValue(preset: "?", curve: "?", force: 0, duration: 0)
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         springView.layer.cornerRadius = 10
+        changingLabelValue()
     }
 
     @IBAction func runAnimaton(_ sender: SpringButton) {
+        if sender.titleLabel?.text == "Run animation" {
+            setRandomLabelValue()
+        }
+        changingLabelValue()
+        if delayValue.text == "0" {
+            delayValue.text = String(format: "%.0f", springView.delay)
+        }
+        
+        changingAnimationValue()
+        
+        setRandomLabelValue()
+        
+        sender.setTitle("Run \(labelValue.preset)", for: .normal)
+    }
+    private func changingLabelValue() {
+        presetValue.text = labelValue.preset
+        curveValue.text = labelValue.curve
+        forceValue.text = String(format: "%.2f", labelValue.force)
+        durationValue.text = String(format: "%.2f", labelValue.duration)
+    }
+    
+    private func changingAnimationValue() {
+        springView.animation = labelValue.preset
+        springView.curve = labelValue.curve
+        springView.force = labelValue.force
+        springView.duration = labelValue.duration
+        springView.animate()
+    }
+    
+    private func setRandomLabelValue() {
+        labelValue.preset = nameAnimations.shuffled().first ?? ""
+        labelValue.curve = nameCurves.randomElement() ?? ""
+        labelValue.force = CGFloat.random(in: 0...2)
+        labelValue.duration = CGFloat.random(in: 0.5...1.5)
     }
     
 }
